23540077f16e4a675eea94c44fa44a43d07c1d37
Ilja Iwas <ilja@iwascoding.com>
2015-10-09 17:07:17 +0200

    Initial Commit

---
+++ CocoaButter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocoaButter
+//
+//  Created by ilja on 09.10.15.
+//  Copyright © 2015 iwascoding GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
